a3a68b90e53fe74b3268d8edd1f0927c3ddc2b2a
edouardsouan <souan.edouard0@gmail.com>
2014-05-10 19:55:30 +0200

    Ajout du MCV (caca)

--- src/main/java/controlleur/ControlleurPersonne.java
+++ src/main/java/controlleur/ControlleurPersonne.java
@@ -1,5 +1,11 @@
 package controlleur;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+
 public class ControlleurPersonne {
 
 }
--- src/main/java/modele/Personne.java
+++ src/main/java/modele/Personne.java
@@ -1,15 +1,25 @@
+package modele;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.validation.constraints.*;
+
 /**
  * Created by edouard alexandre on 10/05/14.
  *
  */
 //package fr.epsi;
+
+
 @Named
 @RequestScoped
 public class Personne {
+    @Size(min = 1, message = "Le nom est obligatoire !")
     private String nom;
+
+    @Min(value=1, message = "L'âge doit être un nombre positif !")
+    @Max(value=99, message = "L'âge ne peut pas dépasser 99 ans !")
+
     private int age;
 
     public String getNom() {
